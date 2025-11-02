9d58bd17a95b1e7585960a10d1d8888d5aa8290f
Marius <privacyfirst555@proton.me>
2025-11-03 01:30:40 +0200

    Allow TOTP code to be used only once.

--- app/controllers/users/login.py
+++ app/controllers/users/login.py
@@ -2,7 +2,8 @@
 import bcrypt
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from models.users.user import check_length, get_credentials, mark_login
-from totp import verify
+from totp import verify, period
+from datetime import datetime, timedelta
 
 login_bp = Blueprint('login', __name__)
 
@@ -32,7 +33,9 @@ def login():
             return render_template('users/login.html')
 
         password_is_valid = bcrypt.checkpw(password.encode('utf-8'), response['password'].encode('utf-8'))
-        twofa_is_valid = not response['twofa_secret'] or verify(totp, response['twofa_secret'])
+        new_totp = datetime.utcnow() - response['last_login'] > timedelta(seconds=period)
+        twofa_is_valid = not response['twofa_secret'] or (new_totp and verify(totp, response['twofa_secret']))
+        print(password_is_valid, new_totp, twofa_is_valid)
         if password_is_valid and twofa_is_valid:
             if (err := mark_login(username, True)):
                 flash(err, "error")
@@ -40,8 +43,10 @@ def login():
             session['username'] = username
             session['role'] = 'admin' if response['admin'] else 'user'
             return redirect(url_for('home.home'))
-
-        flash("Invalid credentials, please try again.", "error")
+        if new_totp:
+            flash("Invalid credentials, please try again.", "error")
+        else:
+            flash(f"The 2FA code has been used up, wait {period}s.", "error")
         if (err := mark_login(username, False)):
             flash(err, "error")
             return render_template('users/login.html')
