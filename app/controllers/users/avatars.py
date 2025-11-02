667415501bae9c489ffa9f47a2bf67ecb9e1407e
Marius <privacyfirst555@proton.me>
2025-11-02 18:49:50 +0200

    Add avatar managment backend.

--- app/controllers/users/avatars.py
+++ app/controllers/users/avatars.py
@@ -1,6 +1,6 @@
 # controllers/users/avatars.py
 from flask import Blueprint, render_template, redirect, url_for, session, flash, request
-from models.users.avatar import get_avatars
+from models.users.avatar import get_avatars, add, remove, check_length
 
 avatars_bp = Blueprint('avatars', __name__)
 
@@ -9,11 +9,30 @@ def avatars():
     if session.get('role', 'guest') != 'admin':
         flash(f"You do not have permisions to access that page.", "error")
         return redirect(url_for('home.home'))
+
     avatars = get_avatars()
-    return render_template('users/avatars.html', avatars=avatars)
+    if request.method == 'GET':
+        return render_template('users/avatars.html', avatars=avatars)
+    elif request.method == 'POST':
+        name = request.form.get('name')
+        url = request.form.get('url')
+        if (err := check_length('name', name)):
+            flash(f"Avatar name must be between {err} characters.", "error")
+        elif (err := check_length('url', url)):
+            flash(f"Avatar URL must be between {err} characters.", "error")
+        elif (err := add(name, url)):
+            flash(f"{err}", "error")
+        return redirect(url_for('avatars.avatars'))
 
 @avatars_bp.route('/remove_avatar', methods=['POST'])
 def remove_avatar():
+    if session.get('role', 'guest') != 'admin':
+        flash(f"You do not have permisions to access that page.", "error")
+        return redirect(url_for('home.home'))
+
     name = request.form.get('avatar_name')
-    flash(f"Removed avatar: {name}", "success")
+    if (err := remove(name)):
+        flash(f"{err}", "error")
+    else:
+        flash(f"Removed avatar: {name}", "success")
     return redirect(url_for('avatars.avatars'))
--- app/models/users/avatar.py
+++ app/models/users/avatar.py
@@ -1,5 +1,6 @@
 # models/users/avatar.py
-from models.db import get_one, get_all
+from models.db import get_one, get_all, execute
+import psycopg2
 
 AVATAR_COLUMN_LENGTHS = {
     "name": [3, 30],
@@ -10,6 +11,34 @@ def check_length(key, value):
     if AVATAR_COLUMN_LENGTHS[key][0] <= len(value) <= AVATAR_COLUMN_LENGTHS[key][1]:
         return False
     return f"{AVATAR_COLUMN_LENGTHS[key][0]} and {AVATAR_COLUMN_LENGTHS[key][1]}"
+
+def add(name, url):
+    try:
+        execute("""
+            INSERT INTO avatars (name, url)
+            VALUES (%s, %s)
+        """, (name, url))
+    except psycopg2.errors.UniqueViolation:
+        return "Name or URL already exists."
+
+    except psycopg2.errors.StringDataRightTruncation:
+        return "Invalid input length. Please check name and URL."
+
+    except Exception as e:
+        print(f"Unknown error: {e}")
+        return "Unknown error."
+
+def remove(name):
+    try:
+        execute("""
+             DELETE FROM avatars WHERE name = %s
+        """, (name, ))
+    except psycopg2.errors.ForeignKeyViolation:
+        return "There are users using this avatar."
+
+    except Exception as e:
+        print(f"Unknown error: {e}")
+        return "Unknown error."
 
 def get_avatar(username):
     try:
@@ -28,7 +57,7 @@ def get_avatar(username):
 
 def get_avatars():
     try:
-        avatars = get_all("""SELECT a.name, a.url, a.id FROM avatars a""")
+        avatars = get_all("""SELECT a.name, a.url, a.id FROM avatars a ORDER BY a.id DESC""")
         if avatars:
             return avatars
         return []
