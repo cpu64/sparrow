3bc2691787d372e8bdec86dd179a0c979e10234f
pppss102 <73379683+pppss102@users.noreply.github.com>
2025-11-09 18:00:34 +0200

    add mock frontend for dms

--- app/controllers/dms/dms.py
+++ app/controllers/dms/dms.py
@@ -1,7 +1,5 @@
-# direct messages (^_^)
-
+from flask import Blueprint, flash, redirect, render_template, session, url_for
 import json
-from flask import Blueprint, flash, redirect, session, url_for
 
 dms_blueprint = Blueprint('dms', __name__)
 
@@ -12,4 +10,86 @@ def dms():
         flash(f"You do not have permisions to access that page.", "error")
         return redirect(url_for('home.home'))
 
-    return f""
+    # for now, mock data instead of fetching from database
+    # (will also need to fetch user id from session)
+    chat_dump = """
+    [
+        {
+            "id": 1,
+            "title": "Vienas pokalbis"
+        },
+        {
+            "id": 2,
+            "title": "Keistas pokalbis"
+        },
+        {
+            "id": 3,
+            "title": "Ne pokalbis"
+        }
+    ]
+    """
+    chats = json.loads(chat_dump)
+
+    return render_template('dms/dm_list.html', chats=chats)
+
+@dms_blueprint.route('/chat/<chat_id>', methods=['GET'])
+def fetch_chat(chat_id):
+    role = session.get("role", "")
+    if role == "guest":
+        flash(f"You do not have permisions to access that page.", "error")
+        return redirect(url_for('home.home'))
+
+    chat_dump = """
+[
+    [
+        {
+            "sender": "A",
+            "content": "labas"
+        },
+        {
+            "sender": "B",
+            "content": "ate"
+        },
+        {
+            "sender": "A",
+            "content": "Nemandagu!"
+        }
+    ],
+    [
+        {
+            "sender": "A",
+            "content": "labas"
+        },
+        {
+            "sender": "A",
+            "content": "labas!!!"
+        },
+        {
+            "sender": "B",
+            "content": "Nemandagu!"
+        }
+    ],
+    [
+        {
+            "sender": "A",
+            "content": "ate"
+        },
+        {
+            "sender": "B",
+            "content": "o kur labas?"
+        },
+        {
+            "sender": "B",
+            "content": "sakiau ate"
+        },
+        {
+            "sender": "A",
+            "content": "miau"
+        }
+    ]
+]
+"""
+
+    messages = json.loads(chat_dump)[int(chat_id) - 1]
+
+    return render_template('dms/chat.html', chat_id=chat_id, messages=messages)
