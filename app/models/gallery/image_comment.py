7de97ba721ff0b910362da50094bcb5c41c82fa8
Marius <privacyfirst555@proton.me>
2025-11-10 08:16:39 +0200

    Unify comment sections for posts and photos by turning them into a template.

--- app/models/gallery/image_comment.py
+++ app/models/gallery/image_comment.py
@@ -1,10 +1,12 @@
 __image_comments = [
     {
-        "name": "Amazing!",
+        "text": "Amazing!",
+        "author": "admin",
         "created_at": "2025-11-01",
     },
     {
-        "name": "RAID: Shadow Legends™ is an immersive online experience with everything you'd expect from a brand new RPG title. It's got an amazing storyline, awesome 3D graphics, giant boss fights, PVP battles, and hundreds of never before seen champions to collect and customize.",
+        "text": "RAID: Shadow Legends™ is an immersive online experience with everything you'd expect from a brand new RPG title. It's got an amazing storyline, awesome 3D graphics, giant boss fights, PVP battles, and hundreds of never before seen champions to collect and customize.",
+        "author": "aaa",
         "created_at": "2025-11-01",
     },
 ]
