e37eea416337371189f067797f3b3d8060f7ab33
ignas24 <ignaspyrantas@gmail.com>
2025-12-14 16:00:21 +0200

    dummy data for posts

--- db/init/init.sql
+++ db/init/init.sql
@@ -105,3 +105,89 @@ INSERT INTO users (
         FALSE,
         3
     );
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    content VARCHAR(500) NOT NULL,
+    text_color VARCHAR(7) NOT NULL DEFAULT '#ffffff',
+    text_font INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2'),
+    views INT NOT NULL DEFAULT 0,
+    user_id INT NOT NULL REFERENCES users(id)
+);
+INSERT INTO posts (content, text_color, text_font, user_id, created_at)
+VALUES
+('Just finished a great book on web development. Highly recommend it!', '#7a7a7a', 1, 2, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '1 minutes'),
+('Exploring the city today!', '#ff5733', 2, 3, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '2 minutes'),
+('Welcome to Sparrow! This is my first post.', '#33ff57', 1, 2, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '3 minutes'),
+('TEST1', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '4 minutes'),
+('TEST2', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '5 minutes'),
+('TEST3', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '6 minutes'),
+('TEST4', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '7 minutes'),
+('TEST5', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '8 minutes'),
+('TEST6', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '9 minutes'),
+('TEST7', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '10 minutes'),
+('TEST8', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '11 minutes'),
+('TEST9', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '12 minutes'),
+('TEST10', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '13 minutes'),
+('TEST11', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '14 minutes'),
+('TEST12', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '15 minutes'),
+('TEST13', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '16 minutes'),
+('TEST14', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '17 minutes'),
+('TEST15', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '18 minutes'),
+('TEST16', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '19 minutes'),
+('TEST17', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '20 minutes'),
+('TEST18', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '21 minutes'),
+('TEST19', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '22 minutes'),
+('TEST20', '#ffffff', 1, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '23 minutes');
+
+
+CREATE TABLE IF NOT EXISTS post_comments (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    content VARCHAR(500) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2'),
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    post_id INT NOT NULL REFERENCES posts(id) ON DELETE CASCADE
+);
+INSERT INTO post_comments (content, user_id, post_id, created_at)
+VALUES
+('Great to be here!', 3, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '30 seconds'),
+('Welcome aboard!', 2, 1, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '20 seconds'),
+('Enjoy your time exploring the city!', 2, 2, (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2') - INTERVAL '25 seconds');
+
+CREATE TABLE IF NOT EXISTS tags (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(63) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC+2')
+);
+INSERT INTO tags (name)
+VALUES
+('Travel'),
+('Photography'),
+('Books'),
+('Technology');
+
+CREATE TABLE IF NOT EXISTS post_tags (
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, tag_id)
+);
+INSERT INTO post_tags (post_id, tag_id)
+VALUES
+((SELECT id FROM posts WHERE content = 'Exploring the city today!'), (SELECT id FROM tags WHERE name = 'Travel')),
+((SELECT id FROM posts WHERE content = 'Exploring the city today!'), (SELECT id FROM tags WHERE name = 'Photography')),
+((SELECT id FROM posts WHERE content = 'Just finished a great book on web development. Highly recommend it!'), (SELECT id FROM tags WHERE name = 'Books')),
+((SELECT id FROM posts WHERE content = 'Just finished a great book on web development. Highly recommend it!'), (SELECT id FROM tags WHERE name = 'Technology')),
+((SELECT id FROM posts WHERE content = 'Welcome to Sparrow! This is my first post.'), (SELECT id FROM tags WHERE name = 'Technology'));
+
+CREATE TABLE IF NOT EXISTS text_fonts(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+INSERT INTO text_fonts (name)
+VALUES
+('Roboto'),
+('Pacifico'),
+('Monoton')
+ON CONFLICT (name) DO NOTHING;
