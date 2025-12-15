a76b4919a300bcf867ebf91cc608901ecaa553a0
Marius <privacyfirst555@proton.me>
2025-12-15 02:27:17 +0200

    Make homepage look slightly better and allow guests to access it.

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -5,6 +5,4 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    if session.get('role', 'guest') == 'guest':
-        return redirect(url_for('login.login'))
-    return render_template('home.html', username=session['username'], role=session['role'])
+    return render_template('home.html', username=session.get('username', 'Guest'), role=session.get('role', 'guest'))
